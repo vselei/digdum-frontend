806c7df424d92bf417178effb985bc045cfc246f
Victor Seleimend <vsennaselei@gmail.com>
2023-03-07 11:48:18 -0300

    logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import { Form } from 'react-router-dom';
 
+import Logo from '/img/digdum-logo.svg';
+
 import Head from '../components/Head';
 import FormInput from '../components/FormInput';
 
@@ -10,6 +12,7 @@ const Login = () => {
         title="Login"
         description="Realize seu login ou crie sua conta, e comece a postar fotos, vídeos, digdums e muito mais. A rede social que ajuda você a crescer! Tudo em um, não perca mais tempo, e use o DigDum."
       />
+      <img src={Logo} alt="Logo DigDum: Um panda branco e azul escuro, segurando um celular" width={480} />
       <Form method="post">
         <FormInput
           id="email"
