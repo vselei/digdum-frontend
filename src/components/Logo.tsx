8056300971c979c3e6072a7a132ce93abc8109d0
Victor Seleimend <vsennaselei@gmail.com>
2023-03-08 12:19:37 -0300

    otimizando logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,24 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-const Logo = styled.img`
+import WhiteThemeLogo from '/img/digdum-logo.svg';
+import DarkThemeLogo from '/img/digdum-logo2.svg';
+
+import useThemes from '../hooks/useThemes';
+
+const ImageLogo = styled.img`
   max-width: 100%;
 `;
+
+const Logo = () => {
+  const { theme } = useThemes();
+
+  return (
+    <ImageLogo
+      src={theme === 'dark' ? DarkThemeLogo : WhiteThemeLogo}
+      alt="Logo DigDum: Um panda branco e azul escuro, segurando um celular"
+      width={480}
+    />
+  );
+};
 
 export default Logo;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,5 @@
 import { Form } from 'react-router-dom';
 
-import WhiteThemeLogo from '/img/digdum-logo.svg';
-import DarkThemeLogo from '/img/digdum-logo2.svg';
-
 import Head from '../components/Head';
 import FormInput from '../components/FormInput';
 import Logo from '../components/Logo';
@@ -10,11 +7,8 @@ import Button from '../components/Button';
 import Anchor from '../components/Anchor';
 import FlexContainer from '../components/FlexContainer';
 import Heading from '../components/Heading';
-import useThemes from '../hooks/useThemes';
 
 const Login = () => {
-  const { theme } = useThemes();
-
   return (
     <>
       <Head
@@ -30,11 +24,7 @@ const Login = () => {
         flex="1 1 24rem"
         minHeight="var(--h-100)"
       >
-        <Logo
-          src={theme === 'dark' ? DarkThemeLogo : WhiteThemeLogo}
-          alt="Logo DigDum: Um panda branco e azul escuro, segurando um celular"
-          width={480}
-        />
+        <Logo />
         <Form method="post">
           <Heading>Login</Heading>
           <FormInput
@@ -60,8 +50,8 @@ const Login = () => {
             width="var(--size-full)"
             maxWidth="var(--resolution-480)"
           >
-            <Anchor path='sign-up'>Não tem uma conta? Cadastre-se!</Anchor>
-            <Anchor path='forgot-password'>Esqueceu sua senha?</Anchor>
+            <Anchor path="sign-up">Não tem uma conta? Cadastre-se!</Anchor>
+            <Anchor path="forgot-password">Esqueceu sua senha?</Anchor>
           </FlexContainer>
         </Form>
       </FlexContainer>
