b4c8e87ab4f532808500d38aa60e86fd122cc517
Victor Seleimend <vsennaselei@gmail.com>
2023-03-09 17:10:21 -0300

    more username possibilities

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -23,7 +23,6 @@ import usernameGenerator from '../utilities/usernameGenerator';
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   // TODO: Página 404 quando a rota não existir
   // TODO: Detectar quando a etapa de cadastro estiver no meio do caminho e encaminhar para primeira etapa, caso não haja dado anterior inserido
-  // TODO: Botão para gerar user aleatório
 
   const formData = await request.formData();
   const inputs = Object.fromEntries(formData);
--- src/utilities/usernameGenerator.ts
+++ src/utilities/usernameGenerator.ts
@@ -3,7 +3,12 @@ import randNumber from './randNumber';
 const usernameGenerator = (name: string) => {
   const lowerCaseName = name.toLowerCase();
 
-  let usernamePossibilities = [lowerCaseName];
+  let usernamePossibilities = [
+    lowerCaseName,
+    `${lowerCaseName}_`,
+    `_${lowerCaseName}`,
+    `${lowerCaseName}${randNumber(0, 1000)}`
+  ];
 
   if (lowerCaseName.trim().includes(' ')) {
     const splittedName = lowerCaseName.split(' ');
@@ -11,6 +16,7 @@ const usernameGenerator = (name: string) => {
     usernamePossibilities = [
       splittedName.join('-'),
       splittedName.join('.'),
+      splittedName.join('_'),
       splittedName
         .map((name, index, array) =>
           index === array.length - 1 ? name : array[index][0]
@@ -18,7 +24,11 @@ const usernameGenerator = (name: string) => {
         .join(''),
       splittedName.reverse().join('-'),
       splittedName.reverse().join('.'),
-      `${splittedName[splittedName.length - 1]}${splittedName[0][0]}`
+      splittedName.reverse().join('_'),
+      `${splittedName[splittedName.length - 1]}${splittedName[0][0]}`,
+      `${splittedName[splittedName.length - 1]}${randNumber(0, 1000)}`,
+      `_${splittedName[splittedName.length - 1]}${randNumber(0, 1000)}`,
+      `${splittedName[splittedName.length - 1]}${randNumber(0, 1000)}_`
     ];
   }
 
