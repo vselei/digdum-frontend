00f45da88b2f9563dc388c6b5d694c4ff23b7d16
Victor Seleimend <vsennaselei@gmail.com>
2023-03-09 20:23:38 -0300

    more username possibilities

--- src/utilities/usernameGenerator.ts
+++ src/utilities/usernameGenerator.ts
@@ -24,9 +24,22 @@ const usernameGenerator = (name: string) => {
           index === array.length - 1 ? name : array[index][0]
         )
         .join(''),
+      splittedName
+        .map((name, index, array) =>
+          index === array.length - 1 ? array[index][0] : name
+        )
+        .join(''),
+      splittedName
+        .map((name, index, array) =>
+          index === array.length - 1 || index === 0 ? array[index][0] : name
+        )
+        .join(''),
       splittedName.reverse().join('-'),
       splittedName.reverse().join('.'),
       splittedName.reverse().join('_'),
+      `${splittedName[splittedName.length - 1]}`,
+      `${splittedName[splittedName.length - 1]}_`,
+      `_${splittedName[splittedName.length - 1]}`,
       `${splittedName[splittedName.length - 1]}${splittedName[0][0]}`,
       `${splittedName[splittedName.length - 1]}${randNumber(0, 1000)}`,
       `_${splittedName[splittedName.length - 1]}${randNumber(0, 1000)}`,
