6a525e5cd6d82fa18514d9647c097bb2fadd75c3
Victor Seleimend <vsennaselei@gmail.com>
2023-03-09 11:42:18 -0300

    forgot password form

---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,56 @@
+import { Form } from 'react-router-dom';
+
+import Head from '../components/Head';
+import FormInput from '../components/FormInput';
+import Logo from '../components/Logo';
+import Button from '../components/Button';
+import Anchor from '../components/Anchor';
+import FlexContainer from '../components/FlexContainer';
+import Heading from '../components/Heading';
+
+const ForgotPassword = () => {
+  return (
+    <>
+      <Head
+        title="Esqueceu Sua Senha"
+        description="Esqueceu sua senha? Recupere já, e volte a usar o DigDum, a única rede social, tudo em um!"
+      />
+      <FlexContainer
+        justifyContent="space-between"
+        alignItems="center"
+        gap={{
+          col: 'var(--size-3)'
+        }}
+        flex="1 1 var(--resolution-240)"
+        minHeight="var(--h-100)"
+      >
+        <Logo />
+        <Form method="post">
+          <Heading>Esqueceu Sua Senha</Heading>
+          <FormInput
+            id="email"
+            label="E-mail"
+            type="email"
+            placeholder="Digite seu e-mail"
+          />
+          <Button type="submit">Recuperar Conta</Button>
+          <FlexContainer
+            justifyContent="space-between"
+            alignItems="center"
+            gap={{
+              row: 'var(--size-1)',
+              col: 'var(--size-1)'
+            }}
+            width="var(--size-full)"
+            maxWidth="var(--resolution-480)"
+          >
+            <Anchor path="sign-up">Não tem uma conta? Cadastre-se!</Anchor>
+            <Anchor path="login">Já possui uma conta? Faça seu Login</Anchor>
+          </FlexContainer>
+        </Form>
+      </FlexContainer>
+    </>
+  );
+};
+
+export default ForgotPassword;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import AuthLayout from '../layouts/AuthLayout';
 import Login from '../pages/Login';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import { ThemesProvider } from '../context/ThemesProvider';
 import SignUp from '../pages/SignUp';
@@ -27,6 +28,10 @@ const routes = createBrowserRouter([
           {
             path: '/sign-up',
             element: <SignUp />
+          },
+          {
+            path: '/forgot-password',
+            element: <ForgotPassword />
           }
         ]
       }
