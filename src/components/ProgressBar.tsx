8aa2e6d687893213ff7311915a945b7322e49984
Victor Seleimend <vsennaselei@gmail.com>
2023-03-10 16:46:35 -0300

    bug fixes

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import { useEffect } from 'react';
+import useAlert from '../hooks/useAlert';
+import AlertType from '../utilities/AlertEnum';
+
+const ProgressBarStyles = styled.div`
+  height: 4px;
+  background-color: var(--white-color);
+  animation: fillSize 5s forwards 1 linear;
+
+  @keyframes fillSize {
+    from {
+      width: 0;
+    }
+
+    to {
+      width: var(--w-100);
+    }
+  }
+`;
+
+const ProgressBar = () => {
+  const { showAlert } = useAlert();
+
+  useEffect(() => {
+    const progressTimer = setTimeout(() => {
+      showAlert({ type: AlertType.Error, message: '' });
+    }, 5000);
+
+    return () => {
+      clearTimeout(progressTimer);
+    };
+  }, []);
+
+  return <ProgressBarStyles />;
+};
+
+export default ProgressBar;
